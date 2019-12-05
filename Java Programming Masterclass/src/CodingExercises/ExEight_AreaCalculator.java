6bd2335659ee78852f4ff29c85040d3217cf3e68
Haywad Yusufzai <Haywad Yusufzai@Haywad-Yusufzai.fios-router.home>
2019-12-04 23:19:26 -0500

    ExEight and ExNine

--- Java Programming Masterclass/src/CodingExercises/ExEight_AreaCalculator.java
+++ Java Programming Masterclass/src/CodingExercises/ExEight_AreaCalculator.java
@@ -4,7 +4,31 @@ public class ExEight_AreaCalculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		double areaCircle = area(50);
+		double areaRectangle = area(5, 3);
+		System.out.println("The area of the circle is " + areaCircle + "u");
+		System.out.println("The area of the Rectangle is " + areaRectangle + "u");
+	}
+	
+	public static double area(double radius) {
+		
+		if (radius < 0) {
+			return -1;
+		}
+		
+		double areaCircle = Math.PI * radius * radius;
+		return areaCircle;
+	}
+	
+	public static double area(double x, double y) {
+		
+		if (x < 0 || y < 0) {
+			return -1;
+		}
+		
+		double areaRectangle = x * y;
+		return areaRectangle;
 	}
 
 }
--- Java Programming Masterclass/src/CodingExercises/ExNine_MinutesToYearsAndDaysCalculator.java
+++ Java Programming Masterclass/src/CodingExercises/ExNine_MinutesToYearsAndDaysCalculator.java
@@ -4,7 +4,22 @@ public class ExNine_MinutesToYearsAndDaysCalculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		long minutes = -525600;
+		printYearsAndDays(minutes);
+	}
+	
+	public static void printYearsAndDays(long minutes) {
+		
+		if (minutes < 0) {
+			System.out.println("Invalid Value");
+		}
+		else {
+		long hours = minutes / 60;
+		long days = hours / 24;
+		long years = days / 365;
+		long remainingDays = days % 365;
+		System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
+		}
 	}
 
 }
