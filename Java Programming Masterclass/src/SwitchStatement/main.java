aebce884f5fea344df663462a2f76af4f1f61745
Haywad Yusufzai <Haywad Yusufzai@Haywad-Yusufzai.fios-router.home>
2019-12-06 00:14:13 -0500

    SwitchStatement

---
+++ Java Programming Masterclass/src/SwitchStatement/main.java
@@ -0,0 +1,24 @@
+package SwitchStatement;
+
+public class main {
+	
+	public static void main(String[] args) {
+		
+		int switchValue = 8;
+		
+		switch(switchValue) {
+			case 1:
+				System.out.println("Value was 1");
+				break;
+			case 2:
+				System.out.println("Value was 2");
+				break;
+			case 3:case 4:case 5:
+				System.out.println("Value is " + switchValue);
+				break;
+			default:
+				System.out.println("Was not 1, 2, 3, 4 or 5");
+				break;
+		}
+	}
+}
