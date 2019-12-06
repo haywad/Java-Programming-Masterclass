61c263814d502701a06f2cdf2fabf466e4ff787f
Haywad Yusufzai <Haywad Yusufzai@Haywad-Yusufzai.fios-router.home>
2019-12-06 00:25:03 -0500

    SwitchStatement

--- Java Programming Masterclass/src/SwitchStatement/main.java
+++ Java Programming Masterclass/src/SwitchStatement/main.java
@@ -20,5 +20,39 @@ public class main {
 				System.out.println("Was not 1, 2, 3, 4 or 5");
 				break;
 		}
+		
+		char switchValue1 = 'F';
+		
+		switch(switchValue1) {
+			case 'A':
+				System.out.println("Value was A");
+				break;
+			case 'B':
+				System.out.println("Value was B");
+				break;
+			case 'C':case 'D':case 'E':
+				System.out.println("Value was " + switchValue1);
+				break;
+			default:
+				System.out.println("Was not A, B, C, D or E");
+				break;
+		}
+		
+		String switchValue2 = "jANuary";
+		
+		switch(switchValue2.toLowerCase()) {
+			case "january":
+				System.out.println("Value was January");
+				break;
+			case "february":
+				System.out.println("Value was February");
+				break;
+			case "march":case "april":case "may":
+				System.out.println("Value was " + switchValue2);
+				break;
+			default:
+				System.out.println("Not Sure");
+				break;
+		}
 	}
 }
