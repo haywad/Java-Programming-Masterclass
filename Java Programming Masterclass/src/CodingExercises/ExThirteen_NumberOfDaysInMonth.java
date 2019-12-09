32de34a43c9601358df5654b7006af1ca4200be4
Haywad Yusufzai <Haywad Yusufzai@Haywad-Yusufzai.fios-router.home>
2019-12-09 13:59:14 -0500

    ForStatement

---
+++ Java Programming Masterclass/src/CodingExercises/ExThirteen_NumberOfDaysInMonth.java
@@ -0,0 +1,20 @@
+package CodingExercises;
+
+public class ExThirteen_NumberOfDaysInMonth {
+
+	public static void main(String[] args) {
+		
+		boolean ans = isLeapYear(5);
+		System.out.println("Leap Year = " + ans);
+		getDaysInMonth(25, 18);
+	}
+	
+	public static boolean isLeapYear(int year) {
+		
+	}
+	
+	public static int getDaysInMonth(int month, int year) {
+		
+	}
+
+}
---
+++ Java Programming Masterclass/src/ForStstement/main.java
@@ -0,0 +1,9 @@
+package ForStstement;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
