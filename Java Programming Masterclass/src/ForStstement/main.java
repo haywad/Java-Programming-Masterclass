78b19ad76a8c49504f9720597e8089ff1c21325d
Haywad Yusufzai <Haywad Yusufzai@Haywad-Yusufzai.fios-router.home>
2019-12-13 12:05:51 -0500

    ForStatement

--- Java Programming Masterclass/src/ForStstement/main.java
+++ Java Programming Masterclass/src/ForStstement/main.java
@@ -3,7 +3,27 @@ package ForStstement;
 public class main {
 
 	public static void main(String[] args) {
+		System.out.println("10,000 at 2% = " + calculateIntrest(10000.0, 2.0));
 		
+		for (int i = 0; i < 6; i++) {
+			System.out.println("i = " + i);
+		}
+		
+		System.out.println("**********************************************************");
+		
+		for (int i = 2; i < 9; i++) {
+			System.out.println("10,000 at " + i + "% = " + calculateIntrest(10000.0, i));
+		}
+		
+		System.out.println("**********************************************************");
+		
+		for (int i = 8; i >= 2; i--) {
+			System.out.println("10,000 at " + i + "% = " + calculateIntrest(10000.0, i));
+		}
+	}
+	
+	public static double calculateIntrest(double amount, double intrestRate) {
+		return(amount * (intrestRate/100));
 	}
 
 }
