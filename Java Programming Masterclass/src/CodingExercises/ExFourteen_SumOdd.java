143660aef040772b6c341ae24b17e911776b3f41
haywa <haywa@Haywad-Yusufzai.fios-router.home>
2020-09-21 14:06:38 -0400

    ExFourteen

--- Java Programming Masterclass/src/CodingExercises/ExFourteen_SumOdd.java
+++ Java Programming Masterclass/src/CodingExercises/ExFourteen_SumOdd.java
@@ -1,5 +1,34 @@
 package CodingExercises;
 
 public class ExFourteen_SumOdd {
+	
+	public static void main(String[] args) {
+		int ans = sumOdd(-1, 100);
+		System.out.println(ans);
+	}
+	
+	public static boolean isOdd(int number) {
+		
+		if (number <= 0) {
+			return false;
+		}
+		return number % 2 != 0;
+		
+	}
+	    
+	public static int sumOdd(int start, int end){
+
+		if ( (end < start) || (start < 0) || (end < 0) ){
+			return -1;
+		}
+		
+		int sum = 0;
+		for (int i = start; i<=end; i++){
+			if (isOdd(i)){
+				sum +=i;
+			}
+		}
+		return sum;
+	}
 
 }
